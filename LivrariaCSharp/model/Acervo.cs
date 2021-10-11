c13ce6240549273cb2a29ae6a3ecf6ee56ccf239
RagnarCris <cristianonascimento98@gmail.com>
2021-10-11 15:58:46 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaCSharp/model/Acervo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaCSharp.model
+{
+    class Acervo
+    {
+        public int id { get; set; }
+        public int id_editora { get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public int ano { get; set; }
+        public double preco { get; set; }
+        public int quantidade { get; set; }
+        public int tipo { get; set; }
+
+        public Acervo()
+        {
+        }
+        public Acervo(int id, int id_editora, string titulo, string autor, int ano, double preco, int quantidade, int tipo)
+        {
+            this.id = id;
+            this.id_editora = id_editora;
+            this.titulo = titulo;
+            this.autor = autor;
+            this.ano = ano;
+            this.preco = preco;
+            this.quantidade = quantidade;
+            this.tipo = tipo;
+        }
+        public override string ToString()
+        {
+            return this.titulo;
+        }
+    }
+}
---
+++ LivrariaCSharp/model/Editora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaCSharp.model
+{
+    class Editora
+    {
+        private int id;
+        private string nome;
+        public Editora()
+        {
+        }
+        public Editora(int id, string nome)
+        {
+            this.id = id;
+            this.nome = nome;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.nome;
+        }
+    }
+}
